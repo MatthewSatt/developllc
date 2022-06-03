93c0d4ddae942b38d1a49258dbb596b20e4d6ee5
Matthew <MatthewSatterwhiteMS@gmail.com>
2022-06-03 15:13:37 -0700

    mission

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
-import Nav from './components/Nav';
+import "./App.css";
+import Nav from "./components/Nav";
+import TalkWithUs from "./components/TalkWithUs";
 function App() {
   return (
     <div className="App">
       <Nav />
+      <TalkWithUs />
+      <div className="mission">
+        <h6 className="goal">Our Mission</h6>
+        <div className="thegoal">
+          <p>To guide the inexperienced and to enable the experienced to shine through, fulfilling any and all of your online needs.</p>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/TalkWithUs/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./TalkWithUs.css";
+
+function TalkWithUs() {
+  return (
+    <div className="talkwithus">
+      <div className="talkwithuscontent">
+        <h1>Build Better Websites and Software, For Less</h1>
+        <div>DEVELOP L.L.C is a custom website and custom software development agency, which provides hardworking teams of exceptional developers.</div>
+        <div id="ques">Are you a rapidly growing startup or an existing enterprise?</div>
+        <button className="talkwithusbutton">Talk With Us</button>
+
+      </div>
+    </div>
+  );
+}
+
+export default TalkWithUs;
